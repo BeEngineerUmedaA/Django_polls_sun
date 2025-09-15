108c8e36023879865faeddf9bb719d54574d5e22
beengineer-umeda <beengineer.teacher.umeda@gmail.com>
2025-09-15 17:05:40 +0900

    temlates作成

--- main/views.py
+++ main/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("こんにちは！世界！")
-
-def hobby(request):
-    return HttpResponse("私のhobbyは歩くことかもしれません")
-
-def greet(request, name):
-    message = "こんにちは" + name + "さん"
-    return HttpResponse(message)
+    question_list = [
+        "ゲームは好きですか？",
+        "勉強は好きですか？",
+        "あなたは私ですか？",
+    ]
+    context = {
+        "question_list": question_list,
+        "is_polled": True,
+        "polled_msg": "投票ありがとうございました❤️",
+        "not_polled_msg": "さっさと投票してよー泣",
+    }
+    return render(request, "main/index.html", context)
